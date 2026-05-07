5f3a361f6c0a4b0601c202e30df1faa52fcc52d9
Achim Bunke <achim.bunke@googlemail.com>
2026-05-07 18:46:52 +0200

    added missing error msg

--- Model/ErrorCodeToMessage.cs
+++ Model/ErrorCodeToMessage.cs
@@ -10,6 +10,7 @@ public static class ErrorCodeToMessage
         [ErrorCodes.MeshExtractor.MissingMesh] = "Item contains no mesh to extract.",
         [ErrorCodes.MeshExtractor.UnsupportedMesh] = "Mesh to be extracted is not supported. Confirm that mesh is either: CPlugCrystal, CPlug2Solid, MovingItem.",
         [ErrorCodes.MovingItemCreator.MeshExtractionFailed] = "Failed to extract mesh from item.",
+        [ErrorCodes.EmbeddedItemExtractor.MissingEmbeddedData] = "No embedded item data to extract.",
     };
     public static string GetMessage(string errorCode)
     {
